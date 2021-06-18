36bd144c95405a3ee932199f6bdfdc136c0fe3d3
John Walicki <johnwalicki@gmail.com>
2021-06-18 09:52:32 -0400

    Disable projects inside container
    Signed-off-by: John Walicki <johnwalicki@gmail.com>

--- settings.js
+++ settings.js
@@ -270,7 +270,7 @@ module.exports = {
     editorTheme: {
         projects: {
             // To enable the Projects feature, set this value to true
-            enabled: true
+            enabled: false
         }
     }
 };
